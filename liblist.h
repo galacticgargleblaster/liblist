429d456d19d80dc7553ea15222e3c204b504668a
nic james <nic@nicjame.es>
2019-03-20 14:47:30 -0700

    abnormalized liblist.h

--- liblist.h
+++ liblist.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   liblist.h                                          :+:      :+:    :+:   */
+/*   turns_out_this_does_not_matter.h                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: student <student@42.fr>                    +#+  +:+       +#+        */
+/*   By: student <student@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/14 13:42:25 by marvin            #+#    #+#             */
-/*   Updated: 2019/03/18 17:01:22 by student          ###   ########.fr       */
+/*   Created: 1967/08/25 00:00:00 by student           #+#    #+#             */
+/*   Updated: 4242/42/42 66:66:66 by studnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,6 @@
 
 #ifndef LIBLIST_H
 # define LIBLIST_H
-
 # include <stdlib.h>
 
 typedef struct					element_container_s
@@ -55,10 +54,13 @@ typedef struct					doubly_linked_list_s
 }								doubly_linked_list_t;
 
 int								list_is_empty(doubly_linked_list_t *list);
-void							list_prepend(doubly_linked_list_t *list, void *element);
-void							list_append(doubly_linked_list_t *list, void *element);
+void							list_prepend(
+	doubly_linked_list_t *list, void *element);
+void							list_append(
+	doubly_linked_list_t *list, void *element);
 doubly_linked_list_t			*new_doubly_linked_list();
 element_container_t				*new_doubly_linked_element_container();
-void							delete_doubly_linked_list(doubly_linked_list_t *list, void del(void *element));
+void							delete_doubly_linked_list(
+	doubly_linked_list_t *list, void del(void *element));
 
 #endif
